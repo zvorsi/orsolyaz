489af8778e586776f03c6cce079484ca9756a575
zvorsi <orsolya.zdeborskyvadasz@gmail.com>
2020-01-20 17:40:08 +0100

    word frequency ready

--- ExamPractice/src/WordFrequency/wordFrequency.java
+++ ExamPractice/src/WordFrequency/wordFrequency.java
@@ -4,7 +4,10 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class wordFrequency {
 
@@ -12,9 +15,9 @@ public class wordFrequency {
         wordFrequency(2);
     }
 
-    public static void wordFrequency(int frequency){
+    public static void wordFrequency(int frequency) {
 
-        List<String> lines;
+        List<String> lines = null;
         try {
             Path filePath = Paths.get("src/WordFrequency/input.txt");
             lines = Files.readAllLines(filePath);
@@ -22,6 +25,36 @@ public class wordFrequency {
             System.out.println("file is not read");
             System.exit(2);
         }
-        
+
+        HashMap<String, Integer> wordsByFreqency = new HashMap<>();
+        for (String line : lines) {
+            String[] words = line.split(" ");
+            for (String word : words) {
+                if (wordsByFreqency.containsKey(word)) {
+                    wordsByFreqency.put(word, wordsByFreqency.get(word) + 1);
+                } else {
+                    wordsByFreqency.put(word, 1);
+                }
+            }
+        }
+
+        ArrayList<String> matchingWords = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : wordsByFreqency.entrySet()) {
+            if (entry.getValue() == frequency){
+                matchingWords.add(entry.getKey());
+            }
+        }
+
+        List<String> content = new ArrayList<>();
+        if (matchingWords.size() > 0){
+            content.add(String.join(", ", matchingWords));
+        }
+        try {
+            Path filePath = Paths.get("src/WordFrequency/output.txt");
+            Files.write(filePath, content);
+        } catch (IOException e) {
+            System.out.println("could not write the file");
+            System.exit(1);
+        }
     }
 }
